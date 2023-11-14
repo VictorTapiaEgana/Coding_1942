e43fcfd0a06210aec12cef3b86eb049d2d78d2b0
Victor Tapia <victortapiaegana@gmail.com>
2023-11-14 17:58:49 -0600

    ultimos cambio

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,10 @@ var HERO = document.getElementById('hero')
 var ENEMIGOS = document.getElementById('enemies')
 const VELOCIDAD_DESPLAZAMIENTO  = 30;
 var DESPLAZAMIENTO_ENEMIGOS = 30;
-const VELOCIDAD_RENDERIZADO = 100;
+const VELOCIDAD_RENDERIZADO = 90;
+var PUNTAJE= document.getElementById('score');
+
+PUNTAJE.innerText = 0;
 
 var Hero = {
     vertical : 500,
@@ -37,10 +40,6 @@ function MoverEmenigos (){
 
          if (Enenigos[x].posVertical < 500){
             Enenigos[x].posVertical += DESPLAZAMIENTO_ENEMIGOS;   
-            
-            //COMPROBARA COLICION EN LA POCICION ACTAL
-
-            ///////////////////////////////////////////        
 
          }else{
             ///empezar desde Cero y volver a generar los enemigo de manera aleatoria
@@ -61,6 +60,8 @@ function MoverEmenigos (){
 
     RenderizarEnemigos()
 }
+
+
 
 function RenderizarEnemigos (){
     for (var x =0 ; x < Enenigos.length ; x++){
@@ -85,17 +86,42 @@ function Disparo (){
 
 function AnimarDisparo (){    
     var bala = document.getElementById('bullets')
-    var balazo =''        
-
+    var balazo =''
+    var PosExplosionHor = 0;        
+    var PosExplosionVer = 0;        
 
      for(var x = 0; x < Disparos.length; x++){
         if( Disparos[x].vertical >= 0 ){
             Disparos[x].vertical += - 10; 
-            balazo += `<div id='${Disparos[x].id}' class='bullet' style='top:${Disparos[x].vertical}px; left:${Disparos[x].horizontal}px;'></div>` 
+            balazo += `<div id='${Disparos[x].id}' class='bullet' style='top:${Disparos[x].vertical}px; left:${Disparos[x].horizontal}px;'></div>`     
+        
+                for( var j = 0 ; j < Enenigos.length; j++){
+                    if (Disparos[x].vertical >= Enenigos[j].posVertical &&
+                        Disparos[x].vertical < Enenigos[j].posVertical +  35 && 
+                        Disparos[x].horizontal >= Enenigos[j].posHorizonta &&
+                        Disparos[x].horizontal < Enenigos[j].posHorizonta + 35) { 
+                        
+                        // sumar puntaje
+                        PUNTAJE.innerText = Number(PUNTAJE.innerText)+  10;
+                        
+                        //Repocicionar al enemigo
+                        PosExplosionHor = Enenigos[j].posHorizonta;
+                        PosExplosionVer = Enenigos[j].posVertical;
+                        Enenigos[j].posVertical = -200;    
+                        Enenigos[j].posHorizonta = Math.floor(Math.random() * 850) + 100;    
+                        
+                        //generar la explosion y Sonido                                                  
+                         explosion.style .left = PosExplosionHor;
+                         explosion.style.top  = PosExplosionVer;
+                         explosion.style.backgroundPosition =  '-110px -30px';
+                         SonidoExplosion()
+
+                   }
+                }            
+
+
         }
-        else{
-        console.log('sfsf')
-        }
+
      }
 
      
@@ -103,34 +129,67 @@ function AnimarDisparo (){
     bala.innerHTML = balazo;   
 }
 
+function SonidoExplosion (){
+    const audioElement = new Audio("./assets/boomy-shot-fx.wav");
+    audioElement.play();
+}
+
 //Mover Avion 
 function MoverAvion(e){    
-//   console.log(e.keyCode)
+
     switch (e.keyCode) {
         case 38: //Arriba
-            Hero.vertical -= VELOCIDAD_DESPLAZAMIENTO;                   
+        if((Hero.vertical - VELOCIDAD_DESPLAZAMIENTO) > 0){                    
+            Hero.vertical -= VELOCIDAD_DESPLAZAMIENTO;   
+        }                
             break;
         case 40://abajo
-            Hero.vertical += VELOCIDAD_DESPLAZAMIENTO;                   
+        if((Hero.vertical + VELOCIDAD_DESPLAZAMIENTO) < 550){
+            Hero.vertical += VELOCIDAD_DESPLAZAMIENTO;       
+        }            
              break;
         case 37://izquierda
+        if((Hero.horizontal - VELOCIDAD_DESPLAZAMIENTO) > -15){
             Hero.horizontal -= VELOCIDAD_DESPLAZAMIENTO;
+        }
             break;
         case 39://Derecha
+        if((Hero.horizontal + VELOCIDAD_DESPLAZAMIENTO) < 1000){
             Hero.horizontal += VELOCIDAD_DESPLAZAMIENTO;
+        }
             break;
         case 32: //disparo
             Disparo ()
             break;
     }
+
+    revisarChoque()
+
 }
+
+function revisarChoque() {
+    
+    for (let i = 0; i < Enenigos.length; i++) {
+        //ancho enemigos 35x35
+        //ancho avion 28x28
+        if (Hero.vertical < (Enenigos[i].posVertical + 35) &&
+           (Hero.vertical + 28) > Enenigos[i].posVertical &&
+           Hero.horizontal < (Enenigos[i].posHorizonta + 35) &&
+           (Hero.horizontal + 28) > Enenigos[i].posHorizonta ){
+
+            PUNTAJE.innerText = Number(PUNTAJE.innerText) -  500;
+        }
+    }
+}
+
 
 function gameLoop(){    
     //actualizar la posicion del avion
     DisplayHero()
-    MoverEmenigos()    
-    AnimarDisparo( )
-  
+    //mover enemigos
+    MoverEmenigos() 
+    //animar disparos   
+    AnimarDisparo()  
 }
 
 addEventListener("DOMContentLoaded", () => {
